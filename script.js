ae9ed47b012abae8b80a1c4951ea29b59d483445
William Gulian <williamgulian@gmail.com>
2015-05-29 16:25:52 -0400

    Added multiple and changeable queries.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /*jshint browser:true */
 /*global $:false, jQuery:false, console:false */
+var Query_, queries;
 
 $(function () {
     console.log("JS Loaded");
@@ -9,79 +10,183 @@ $(function () {
     if (location.hash != "" && location.hash != "#")
         query = location.hash.substr(1);
 
-    var imgs = [];
+    function Query(q) {
+        var self = this;
+        this.query = q;
+        this.imgs = [];
+        this.urls = [];
+        var shouldStop = false;
+        this.imagesExpire = true;
 
-    function loadImage(url) {
-        var img = document.createElement("img");
-        img.src = url;
-        img.className = "image";
-        img.onload = function () {
-            console.log("Loaded \"" + url + "\"");
-            img.style.top = "-250px";
+        this.loadImage = function (url) {
+            if (shouldStop)
+                return;
+            var img = document.createElement("img");
+            img.src = url;
+            img.className = "image";
+            img.onload = function () {
+                console.log("Loaded \"" + url + "\"");
+                img.style.top = "-825px";
 
-            imgs.push(img);
+                self.imgs.push(img);
 
-            document.body.appendChild(img);
-        };
-        img.style.left = Math.round(Math.random() * 100) + "%";
-
-    }
-
-    setInterval(function () {
-        for (var i = 0; i < imgs.length; i++) {
-            var img = imgs[i];
-
-            var h = (parseInt(img.style.top.match(/-?\d+/g)) + 1);
-
-            if (h > document.body.clientHeight + 300) {
-                h = -250;
-                img.style.left = Math.round(Math.random() * 100) + "%";
-            }
-
-
-            img.style.top = h + "px";
+                document.body.appendChild(img);
+            };
+            img.style.left = Math.round(Math.random() * 100) + "%";
 
         }
 
-        if (location.hash != "#" + query)
-            location.reload();
+        this.intervalId = setInterval(function () {
+            for (var i = 0; i < self.imgs.length; i++) {
+                var img = self.imgs[i];
 
-    }, 10);
+                var h = (parseInt(img.style.top.match(/-?\d+/g)) + 1);
 
-    function requestJSON(index) {
+                if (h > document.body.clientHeight + 300 || h == -825) {
+                    h = -250 - Math.random(Math.random() * document.body.clientHeight);
+                    img.style.left = Math.round(Math.random() * 100) + "%";
+                    if (self.imagesExpire) {
+                        self.imgs.splice(i, 1);
+                        i--;
 
-        $.ajax({
-                url: "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + escape(query) + "&rsz=8&start=" + index,
-                type: 'GET',
-                crossDomain: true,
-                dataType: 'jsonp'
-            })
-            .done(function (data) {
+                        if (img.parentNode) {
+                            img.parentNode.removeChild(img);
+                        }
 
-                var json = data;
+                        continue;
+                    }
 
-                if (json.responseStatus == 200) {
-                    var images = json.responseData.results;
-
-                    for (var i = 0; i < images.length; i++)(function () {
-                        var img = images[i];
-
-                        setTimeout(function () {
-                            loadImage(img.url);
-
-                        }, (index + i) * Math.round(500 + Math.random() * 500));
-
-                    })();
-
-                    requestJSON(index + 8);
                 }
 
-            });
+
+                img.style.top = h + "px";
+
+            }
+
+
+
+        }, 10);
+
+
+        var requestJSON = function (index) {
+            if (shouldStop)
+                return;
+            console.log("Requesting: " + self.query + "@" + index);
+
+            $.ajax({
+                    url: "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + escape(self.query) + "&rsz=8&start=" + index,
+                    type: 'GET',
+                    crossDomain: true,
+                    dataType: 'jsonp'
+                })
+                .done(function (data) {
+                    if (shouldStop)
+                        return;
+
+
+                    var json = data;
+
+                    if (json.responseStatus == 200) {
+                        var images = json.responseData.results;
+
+                        for (var i = 0; i < images.length; i++)(function () {
+                            var img = images[i];
+
+                            setTimeout(function () {
+                                if (shouldStop)
+                                    return;
+                                self.loadImage(img.url);
+
+                            }, i * Math.round(500 + Math.random() * 1000));
+
+                        })();
+
+                        var waitFunc = function () {
+                            setTimeout(function () {
+
+                                if (document.body.childNodes.length > 35) {
+                                    waitFunc();
+                                } else {
+                                    requestJSON(index + 8);
+                                }
+
+                            }, 8000);
+                        };
+                        waitFunc();
+
+                    }
+
+                });
+
+
+        }
+        requestJSON(0);
+
+        this.stop = function () {
+            shouldStop = true;
+            clearInterval(self.intervalId);
+            for (var i = 0; i < self.imgs.length; i++) {
+                var node = self.imgs[i];
+                if (node.parentNode) {
+                    node.parentNode.removeChild(node);
+                }
+
+            }
+            self.imgs = [];
+        }
 
 
     }
 
-    requestJSON(0);
+    Query_ = Query;
+
+    var queries = [];
+    queries_ = queries;
+
+    var dealWithHash = function (hash) {
+        hash = hash.substr(1);
+        console.log("Hash changed to: " + hash);
+
+        var qStrings = hash.split(";");
+        var existing = [];
+
+        for (var i = 0; i < queries.length; i++) {
+            var query = queries[i];
+            if ($.inArray(query.query, qStrings) == -1) {
+                query.stop();
+
+                queries.splice(i, 1);
+                i--;
+            } else {
+                existing.push(query.query);
+            }
+        }
+
+        console.log("Kept: " + existing);
+
+        for (var i = 0; i < qStrings.length; i++) {
+            var qString = qStrings[i];
+            if ($.inArray(qString, existing) == -1) {
+
+                console.log("Adding: " + qString);
+                queries.push(new Query(qString));
+
+            }
+        }
+    }
+
+    dealWithHash(location.hash);
+
+    $(window).on("hashchange", function () {
+        dealWithHash(location.hash);
+    });
+
+    setInterval(function () {
+        var num = document.body.childNodes.length;
+
+        document.title = "ImageStorm (" + num + ")";
+
+    }, 1000);
 
 
 });
